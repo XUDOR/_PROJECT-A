a7ad51ed4f572717e5969f2c6917ae2f5f60c4ee
Roderick Shoolbraid <rdxenv@gmail.com>
2024-11-24 20:10:43 -0800

    app for express \\ main for DOM

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware for static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic route for serving index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
