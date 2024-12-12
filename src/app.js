48c2924f60f7b660269d12c244c7789df4b9ed74
Roderick Shoolbraid <rdxenv@gmail.com>
2024-12-11 21:29:11 -0800

    F notification

--- src/app.js
+++ src/app.js
@@ -23,6 +23,19 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
+// Notify Project F that Project A is running
+const notifyProjectF = async () => {
+    try {
+        await axios.post('http://localhost:3006/api/notifications', {
+            message: 'Project A is up and running'
+        });
+        console.log('Notified Project F: Project A is running');
+    } catch (error) {
+        console.error('Failed to notify Project F:', error.message);
+    }
+};
+
+
 // Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
